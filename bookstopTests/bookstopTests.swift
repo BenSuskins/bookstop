7f02d182e34cb996e82d453ad078fcb464252906
Ben Suskins <suskinsdevelopment@gmail.com>
2024-10-21 23:14:42 +0100

    Initial Commit

---
+++ bookstopTests/bookstopTests.swift
@@ -0,0 +1,16 @@
+//
+//  bookstopTests.swift
+//  bookstopTests
+//
+//  Created by Ben Suskins on 21/10/2024.
+//
+
+import Testing
+
+struct bookstopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
